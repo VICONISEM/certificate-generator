8d4b2efda01357e5f049b915973acba252ef50b3
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-10-12 00:50:43 +0300

    Update Certificate Generator

--- main.py
+++ main.py
@@ -7,39 +7,42 @@ import tkinter as tk
 from tkinter import filedialog
 
 def select_file(name_of_window):
+    filename = filedialog.askopenfilename(title=f"{name_of_window}")
+    return filename
 
-        filename = filedialog.askopenfilename(title=f"{name_of_window}")
-        return filename
 def select_folder():
-        foldername = filedialog.askdirectory(title="save locatio")
-        return foldername
+    foldername = filedialog.askdirectory(title="Save location")
+    return foldername
 
-def generate_certificate(names, positions, font_of_name, image):
-     save_location =select_folder()   
-     os.chdir(rf"{save_location}")
-     for n, p in zip(names, positions):
+def generate_certificate(names, teams, font_of_name, image):
+    save_location = select_folder()   
+    os.chdir(rf"{save_location}")
+    
+    for n, t in zip(names, teams):
+        team_folder = os.path.join(save_location, t)
+        if not os.path.exists(team_folder):
+            os.makedirs(team_folder)
+
         img_pil = Image.fromarray(image)
         draw = ImageDraw.Draw(img_pil)
-        w,h=draw.textsize(n,font_of_name)
-        draw.text(((img_pil.width-w)//2, 2200), n, font=font_of_name, fill=(0, 0, 0))
-        # draw.text((,), p, font=font_of_rule, fill=(0, 0, 0))
-        image_new = np.array(img_pil)
-        cv2.imwrite(f"certificate for {n}.png", image_new)
-
         
+        bbox = draw.textbbox((0, 0), n, font=font_of_name)
+        w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
+        
+        draw.text(((img_pil.width - w) // 2, 525), n, font=font_of_name, fill=(255, 255, 255))
+        
+        image_new = np.array(img_pil)
+        cv2.imwrite(os.path.join(team_folder, f"certificate_for_{n}.png"), image_new)
 
-certificate_teamplate_path=select_file("select certificate teamplate")
-image = cv2.imread(rf"{certificate_teamplate_path}")
-Excel_sheet_path=select_file("select Excel sheet")
+certificate_template_path = select_file("Select certificate template")
+image = cv2.imread(rf"{certificate_template_path}")
+
+Excel_sheet_path = select_file("Select Excel sheet")
 sheet = pd.read_excel(rf"{Excel_sheet_path}")
-names = list(sheet["name"])
-positions = list(sheet["position"])
+names = list(sheet["NameOfMember"])
+teams = list(sheet["NameOfTeam"])
 
-# position_of_name = cv2.selectROI("select name of participant", image)
-# position_of_rule = cv2.selectROI("select rule of participant", image)
-cv2.destroyAllWindows()
-font_path=select_file("select Font")
-font_of_name = ImageFont.truetype(rf"{font_path}", 300)
-# font_of_rule = ImageFont.truetype(r"C:\Users\victo\OneDrive\Desktop\certificate_generator_python\fonts\times new roman.ttf", 20)
+font_path = select_file("Select Font")
+font_of_name = ImageFont.truetype(rf"{font_path}", 80)
 
-generate_certificate(names, positions, font_of_name, image)
+generate_certificate(names, teams, font_of_name, image)
